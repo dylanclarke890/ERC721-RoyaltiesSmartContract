01182861f2a5e223cf52c022e30a4db670dd4b2e
"Dylan Clarke" <"dylanclarke890@gmail.com">
2022-04-21 21:59:54 +0100

    Added initial contract following ERC-721 with support for secondary royalties

--- raribles/royalties/contracts/IRoyaltiesV2.sol
+++ raribles/royalties/contracts/IRoyaltiesV2.sol
@@ -2,10 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-
 import "./LibPart.sol";
 
-interface RoyaltiesV2 {
+interface IRoyaltiesV2 {
     event RoyaltiesSet(uint256 tokenId, LibPart.Part[] royalties);
 
     function getRaribleV2Royalties(uint256 id) external view returns (LibPart.Part[] memory);
